3c72c5ce630f64061d1bbf595030aaca018cd593
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-06-18 20:26:37 +0200

    Added nconf and config for port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',function(req, res) {
-  console.log(req.path);
+  res.send('Hello');
 });
 
 // app.use('/users', users);
