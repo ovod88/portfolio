acd9eea2b604e2dd529b7f02663fe37a086a1ec5
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-06-28 14:23:17 +0200

    Created working sprite task for multiple folders

--- gulpTasks/sprite.js
+++ gulpTasks/sprite.js
@@ -1,37 +1,58 @@
 const gulp = require('gulp'),
       debug = require('gulp-debug'),
-      sprite = require('gulp.spritesmith');
+      sprite = require('gulp.spritesmith'),
+      path = require('path');
+
 
 module.exports = function(options) {
-    return function () {
-        let spriteData = gulp.src(options.srcFiles, {since: gulp.lastRun(options.taskname)})
-                             .pipe(debug({title: `${options.taskname} compilation`}))
-                             .pipe(sprite({
-                                    imgName: 'icons.png',
-                                    imgPath: 'public/imgs/',
-                                    cssName: '_sprite-icons.scss',
-                                    cssFormat: 'scss',
-                                    algorithm: 'binary-tree',
-                                    padding: 1,
-                                    cssVarMap: function(sprite) {
-                                        var temparray = sprite.name.split('-');
 
-                                        if(temparray[1] == 'blue') {
-                                            sprite.name = temparray[0] + ':hover';
-                                        } else {
-                                            sprite.name = temparray[0];
-                                        }
-                                    },
-                                    cssOpts: {
-                                        cssSelector: function (sprite) {
-                                            return '#' + sprite.name;
-                                        }
-                                    }
-                            }));
 
-        spriteData.img.pipe(gulp.dest(options.imgDst));
-        spriteData.css.pipe(gulp.dest(options.cssDst));
 
-        return spriteData;
-    }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+    // return folders(options.srcFiles, function(folder) {
+
+
+    //             // console.log(path.join(__dirname, "../" + options.srcFiles, folder, '*.png'));
+
+    //             let spriteData = gulp.src(path.join(__dirname, "../" + options.srcFiles, folder, '*.png'), 
+    //                                                                 {since: gulp.lastRun(options.taskname)})
+    //                          .pipe(debug({title: `${path.join(__dirname, "../" + options.srcFiles, folder)} ${options.taskname} compilation`}))
+    //                          .pipe(sprite({
+    //                                 imgName: 'icons.png',
+    //                                 cssName: '_sprite-icons.scss',
+    //                                 cssFormat: 'scss',
+    //                                 algorithm: 'binary-tree',
+    //                                 padding: 1,
+    //                                 cssVarMap: function(sprite) {
+    //                                     var temparray = sprite.name.split('-');
+
+    //                                     if(temparray[1] == 'blue') {
+    //                                         sprite.name = temparray[0] + ':hover';
+    //                                     } else {
+    //                                         sprite.name = temparray[0];
+    //                                     }
+    //                                 },
+    //                                 cssOpts: {
+    //                                     cssSelector: function (sprite) {
+    //                                         return '#' + sprite.name;
+    //                                     }
+    //                                 }
+    //                         }));
+
+    //     spriteData.img.pipe(gulp.dest(options.imgDst + folder));
+    //     spriteData.css.pipe(gulp.dest(options.cssDst + folder));
+
+    //     return spriteData;
+    // });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 'use strict';
 const gulp = require('gulp');
 
+const fs = require('fs'),
+path = require('path'),
+merge = require('merge-stream'),
+sprite = require('gulp.spritesmith'),
+debug = require('gulp-debug');
+
 function lazyTaskRequest(taskName, path, options) {
     options.taskName = taskName;
     gulp.task(taskName, function (callback) {
@@ -37,11 +43,57 @@ lazyTaskRequest('sass', './gulpTasks/sass', {
     dst: 'public/css'
 });
 
-lazyTaskRequest('sprite', './gulpTasks/sprite', {
-    taskname: 'sprite',
-    srcFiles: ['private/imgs/**/*.*'],
-    imgDst: 'public/imgs/',
-    cssDst: 'private/css/'
+// lazyTaskRequest('sprite', './gulpTasks/sprite', {
+//     taskname: 'sprite',
+//     srcFiles: 'private/imgs/',
+//     imgDst: 'public/imgs/',
+//     cssDst: 'private/css/sass/'
+// });
+
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
+
+gulp.task('sprite', function() {
+    var folders = getFolders('private/imgs');
+    
+    var tasks = folders.map(function(folder) {
+
+        let spriteData = gulp.src(path.join('private/imgs', folder, '/**/*.png'))
+                             .pipe(debug({title: `Compilation`}))
+                             .pipe(sprite({
+                                    imgName: 'icons.png',
+                                    cssName: '_sprite-icons.scss',
+                                    cssFormat: 'scss',
+                                    algorithm: 'binary-tree',
+                                    padding: 1,
+                                    cssVarMap: function(sprite) {
+                                        var temparray = sprite.name.split('-');
+
+                                        if(temparray[1] == 'blue') {
+                                            sprite.name = temparray[0] + ':hover';
+                                        } else {
+                                            sprite.name = temparray[0];
+                                        }
+                                    },
+                                    cssOpts: {
+                                        cssSelector: function (sprite) {
+                                            return '#' + sprite.name;
+                                        }
+                                    }
+                            }));
+
+        spriteData.img.pipe(gulp.dest('public/imgs/' + folder));
+        spriteData.css.pipe(gulp.dest('private/css/sass/' + folder));
+
+        return spriteData;
+    })
+
+    return merge(tasks);
 });
 
 lazyTaskRequest('compress-imgs', './tasks/makeImgProd', {
@@ -71,5 +123,6 @@ gulp.task('build-styles-dev', gulp.series('cleanCSS', 'sass', 'watch'));
 gulp.task('build-images', gulp.series('cleanImgs', 'sprite', 'copyfavicon'));
 gulp.task('build-images-dev', gulp.series('cleanImgs', 'sprite', 'copyfavicon'));
 
-// gulp.task('build', gulp.series());//TODO
+gulp.task('build', gulp.series('clean', 'build-images', 'build-styles'));
+gulp.task('build-dev', gulp.series('clean', 'build-images-dev', 'build-styles-dev'));
 
