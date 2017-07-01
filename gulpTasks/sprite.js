1c3a4ebcaa2b7b6b2517534fac5029287ea28f78
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-07-01 13:48:16 +0200

    Added lazy sprite task

--- gulpTasks/sprite.js
+++ gulpTasks/sprite.js
@@ -1,58 +1,56 @@
 const gulp = require('gulp'),
       debug = require('gulp-debug'),
       sprite = require('gulp.spritesmith'),
-      path = require('path');
+      fs = require('fs'),
+      path = require('path'),
+      merge = require('merge-stream');
+
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
 
 
 module.exports = function(options) {
+    return function() {
+        var folders = getFolders(options.src);
+    
+        var tasks = folders.map(function(folder) {
 
+            let spriteData = gulp.src(path.join(options.src, folder, '/**/*.png'))
+                                .pipe(debug({title: `Sprite compilation`}))
+                                .pipe(sprite({
+                                        imgName: 'icons.png',
+                                        imgPath: '../../imgs/' + folder + '/icons.png',
+                                        cssName: '_sprite-icons.scss',
+                                        cssFormat: 'scss',
+                                        algorithm: 'binary-tree',
+                                        padding: 1,
+                                        cssVarMap: function(sprite) {
+                                            var temparray = sprite.name.split('-');
 
+                                            if(temparray[1] == 'blue') {
+                                                sprite.name = temparray[0] + ':hover';
+                                            } else {
+                                                sprite.name = temparray[0];
+                                            }
+                                        },
+                                        cssOpts: {
+                                            cssSelector: function (sprite) {
+                                                return '#' + sprite.name;
+                                            }
+                                        }
+                                }));
 
+            spriteData.img.pipe(gulp.dest(options.dstImg + folder));
+            spriteData.css.pipe(gulp.dest(options.dstCss + folder));
 
+            return spriteData;
+        })
 
-
-
-
-
-
-
-
-
-
-
-    // return folders(options.srcFiles, function(folder) {
-
-
-    //             // console.log(path.join(__dirname, "../" + options.srcFiles, folder, '*.png'));
-
-    //             let spriteData = gulp.src(path.join(__dirname, "../" + options.srcFiles, folder, '*.png'), 
-    //                                                                 {since: gulp.lastRun(options.taskname)})
-    //                          .pipe(debug({title: `${path.join(__dirname, "../" + options.srcFiles, folder)} ${options.taskname} compilation`}))
-    //                          .pipe(sprite({
-    //                                 imgName: 'icons.png',
-    //                                 cssName: '_sprite-icons.scss',
-    //                                 cssFormat: 'scss',
-    //                                 algorithm: 'binary-tree',
-    //                                 padding: 1,
-    //                                 cssVarMap: function(sprite) {
-    //                                     var temparray = sprite.name.split('-');
-
-    //                                     if(temparray[1] == 'blue') {
-    //                                         sprite.name = temparray[0] + ':hover';
-    //                                     } else {
-    //                                         sprite.name = temparray[0];
-    //                                     }
-    //                                 },
-    //                                 cssOpts: {
-    //                                     cssSelector: function (sprite) {
-    //                                         return '#' + sprite.name;
-    //                                     }
-    //                                 }
-    //                         }));
-
-    //     spriteData.img.pipe(gulp.dest(options.imgDst + folder));
-    //     spriteData.css.pipe(gulp.dest(options.cssDst + folder));
-
-    //     return spriteData;
-    // });
+        return merge(tasks);
+    }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,6 @@
 'use strict';
 const gulp = require('gulp');
 
-const fs = require('fs'),
-path = require('path'),
-merge = require('merge-stream'),
-sprite = require('gulp.spritesmith'),
-debug = require('gulp-debug');
-
 function lazyTaskRequest(taskName, path, options) {
     options.taskName = taskName;
     gulp.task(taskName, function (callback) {
@@ -43,57 +37,10 @@ lazyTaskRequest('sass', './gulpTasks/sass', {
     dst: 'public/css'
 });
 
-// lazyTaskRequest('sprite', './gulpTasks/sprite', {
-//     taskname: 'sprite',
-//     srcFiles: 'private/imgs/',
-//     imgDst: 'public/imgs/',
-//     cssDst: 'private/css/sass/'
-// });
-
-
-function getFolders(dir) {
-    return fs.readdirSync(dir)
-      .filter(function(file) {
-        return fs.statSync(path.join(dir, file)).isDirectory();
-      });
-}
-
-gulp.task('sprite', function() {
-    var folders = getFolders('private/imgs');
-    
-    var tasks = folders.map(function(folder) {
-
-        let spriteData = gulp.src(path.join('private/imgs', folder, '/**/*.png'))
-                             .pipe(debug({title: `Compilation`}))
-                             .pipe(sprite({
-                                    imgName: 'icons.png',
-                                    cssName: '_sprite-icons.scss',
-                                    cssFormat: 'scss',
-                                    algorithm: 'binary-tree',
-                                    padding: 1,
-                                    cssVarMap: function(sprite) {
-                                        var temparray = sprite.name.split('-');
-
-                                        if(temparray[1] == 'blue') {
-                                            sprite.name = temparray[0] + ':hover';
-                                        } else {
-                                            sprite.name = temparray[0];
-                                        }
-                                    },
-                                    cssOpts: {
-                                        cssSelector: function (sprite) {
-                                            return '#' + sprite.name;
-                                        }
-                                    }
-                            }));
-
-        spriteData.img.pipe(gulp.dest('public/imgs/' + folder));
-        spriteData.css.pipe(gulp.dest('private/css/sass/' + folder));
-
-        return spriteData;
-    })
-
-    return merge(tasks);
+lazyTaskRequest('sprite', './gulpTasks/sprite', {
+    src: 'private/imgs',
+    dstImg: 'public/imgs/',
+    dstCss: 'private/css/sass/'
 });
 
 lazyTaskRequest('compress-imgs', './tasks/makeImgProd', {
