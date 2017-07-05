eaa7836c8cb7123c43d0db29a22daf12b836e6a0
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-07-05 14:12:53 +0200

    Added babel task for js

--- gulpTasks/babelJS.js
+++ gulpTasks/babelJS.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp'),
       newer = require('gulp-newer'),
-      debug = require('gulp-debug');
+      debug = require('gulp-debug'),
+      gulpIf = require('gulp-if'),
+      combine = require('stream-combiner2').obj,
+      babel = require('gulp-babel');
 
 module.exports = function (options) {
     return function () {
         return gulp.src(options.src, {base: options.base})
                .pipe(newer(options.dst))
-               .pipe(debug({title: 'JS copying ...'}))
+               .pipe(gulpIf(function(file) {
+                    return (file.relative.indexOf('libs') == -1) 
+                            && (file.relative.indexOf('bower_components') == -1);        
+               },
+                 combine(
+                   debug({title: 'Babeling ...'}),
+                   babel({
+                       presets: ['es2015']
+                   })
+               ),
+                 debug({title: 'JS copying ...'})
+               ))
                .pipe(gulp.dest(options.dst));
     }
 }
