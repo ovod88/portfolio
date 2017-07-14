d9f1e8ed9e68897ff3954b7e2bcc447306885e02
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-07-14 13:27:48 +0200

    JSCS checking added in VS extentions

--- private/js/index/main.js
+++ private/js/index/main.js
@@ -1,17 +1,22 @@
 requirejs.config ({
-    baseUrl: '../js',
-    waitSeconds: 200,
-    paths: {
-        'jquery': 'bower_components/jquery/dist/jquery',
-        'lodash': 'index/libs/lodash'
+    baseUrl     : '../js',
+    waitSeconds : 200,
+    paths       : {
+            'jquery' : 'bower_components/jquery/dist/jquery',
+            'lodash' : 'index/libs/lodash'
     }
 });
 
+require( [ 'jquery', 'lodash' ], function ($, _) {
 
-require(['jquery', 'lodash'], function($, _) {
-    $(function() {
-        for(let i = 5; i > 0; i--) {
+    $( function () {
+
+        for( let i = 5; i > 0; i-- ) {
+
             $('.portfolio').fadeToggle('slow');
+
         }
+
     })
+
 });
