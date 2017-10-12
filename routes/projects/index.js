e2a212d2341c3cdc4e28f78a9ee9d1e5095aed89
Khrystenko <volodymyr.khrystenko@atos.net>
2017-10-13 01:21:21 +0200

    Corrected menu for projects which dont have scroll

--- routes/projects/index.js
+++ routes/projects/index.js
@@ -21,10 +21,13 @@ module.exports = (req, res, next) => {
         htmlProject = fs.readFileSync(path.join(options.root, fileName), 'utf8'),
         htmlMenu = fs.readFileSync(path.join(__dirname, '../../', configGulp.dstProjects, 'menu-to-all.html' ), 'utf8'),
         htmlCssMenu = '<link rel="stylesheet" href="../../menu-to-all.css">',
+        htmlFontsAwesome = "<link rel='stylesheet prefetch'" +
+                                    "href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>",
         $ = cheerio.load(htmlProject);
 
         $('body').append(htmlMenu);
         $('head').append(htmlCssMenu);
+        $('head').append(htmlFontsAwesome);
 
         res.send($.html());
 
