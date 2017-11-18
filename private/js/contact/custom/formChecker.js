6a9e7e0c48bf67fcad1fb144bf73a960cb7b122c
Khrystenko <volodymyr.khrystenko@atos.net>
2017-11-18 23:15:08 +0100

    Added form checker in javascript

---
+++ private/js/contact/custom/formChecker.js
@@ -0,0 +1,61 @@
+define([ 'jquery' ], function ( $ ) {
+
+    function checkEmail (value) {
+
+        let regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return value.match(regExp) ? true : false;
+
+    }
+
+    class formChecker {
+
+        constructor(form) {
+
+            this.$form = form;
+            this.$inputs = this.$form.find('.contact_hire_form_input');
+            this.$email = this.$form.find('#client_email');
+            this.$fields = this.$form.find('.contact_hire_form_input, textarea');
+            this.$textArea = this.$form.find('textarea');
+            this.$errors = this.$form.find('.error_message');
+
+        }
+
+        ifVerified () {
+
+            let isOk = true;
+
+            this.$errors.hide();
+            this.$fields.each(function () {
+
+                let current = $(this);
+
+                if (current.val().trim().length === 0) {
+
+                    current.next('.error_message').fadeIn('slow').delay(3000).fadeOut('slow');
+                    isOk = false;
+
+                }
+
+                if (current.is('#client_email') && current.val().trim().length > 0) {
+
+                    if (!checkEmail(current.val().trim())) {
+
+                        current.next('.error_message').fadeIn('slow').delay(3000).fadeOut('slow');
+                        isOk = false;
+
+                    }
+
+                }
+
+            });
+
+            return isOk;
+
+        }
+
+    }
+
+    return formChecker;
+
+});
--- private/js/contact/main.js
+++ private/js/contact/main.js
@@ -1,36 +1,52 @@
-require( [ 'jquery', 'resizeFunc' ], function ($, resizeF) {
+require( [ 'jquery', 'resizeFunc', 'contact/custom/formChecker' ], function ($, resizeF, formChecker) {
 
     resizeF(3);
 
     $('.contact_hire_form_submit').on('click', function (e) {
-            let form = $('.contact_hire_form').serialize(),
+            let $form = $('.contact_hire_form'),
+                formData = $form.serialize(),
                 $preload = $('.preload'),
                 $email_result_message = $('.email_result_message'),
                 $email_result = $('.email_result'),
                 $email_result_cover = $('.email_result_cover'),
-                $email_result_button = $('.email_result_button');
+                $email_result_button = $('.email_result_button'),
+                formCheck = new formChecker($form);
 
             e.preventDefault();
 
-            $preload.delay(200).show();
+            if (formCheck.ifVerified()) {
 
-            $.ajax({
-                url      : "/contact/hire",
-                method   : "POST",
-                data     : form,
-                dataType : "text"
-            }).then(function(msg){
+                $preload.delay(200).show();
+                $.ajax({
+                    url      : "/contact/hire",
+                    method   : "POST",
+                    data     : formData,
+                    dataType : "text"
+                }).then(function (msg){
+
+                    showHideEmailBlock(msg);
+
+                },
+                function (error) {
+
+                    showHideEmailBlock(error);
+
+                });
+
+            }
+
+            function showHideEmailBlock (msg) {
+
                 $preload.hide(100,function() {
                     $email_result_cover.show();
                     $email_result_message.text(msg);
                     $email_result_button.one('click', function() {
+                        $form.find('.contact_hire_form_input, textarea').val('');
                         $email_result_cover.hide();
                     });
                 });
-            },
-            function(error) {
-                alert(error);
-            });
+
+            }
 
         });
 
